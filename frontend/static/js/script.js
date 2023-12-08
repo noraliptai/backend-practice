05e14fa5f5e9c180a1d9d8fd238f9ec1010692fc
Your Name <you@example.com>
2023-12-08 10:05:12 +0100

    added new user

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,3 +1,6 @@
+const rootElement = document.getElementById("root")
+let usersData = []
+
 console.log("loaded")
 
 const formComponent = () => `
@@ -9,28 +12,55 @@ const formComponent = () => `
     </form>
 `
 
-const root = document.getElementById("root")
+const userCardComponent = (userData) => `
+    <div class="card">
+        <h3>${userData.id}</h3>
+        <h2>${userData.name}</h2>
+    </div>
+`
 
-root.insertAdjacentHTML("beforeend", formComponent())
+const init = () => {
+    
 
-/* Megakadályozzuk a form alapvető működését, mert mi akarjuk definiálni hogy hogyan küldje az adatot */
-const formElement = document.querySelector("form")
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault()
+    rootElement.insertAdjacentHTML("beforeend", formComponent())
 
-    const userName = document.querySelector(`input[name="name"]`).value
-    const userPassword = document.querySelector(`input[name="password"]`).value
+    /* Megakadályozzuk a form alapvető működését, mert mi akarjuk definiálni hogy hogyan küldje az adatot */
+    const formElement = document.querySelector("form")
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault()
 
-    fetch("/users/new-user", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: userName,
-            password: userPassword
+        const userName = document.querySelector(`input[name="name"]`).value
+        const userPassword = document.querySelector(`input[name="password"]`).value
+
+        fetch("/users/new-user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: userName,
+                password: userPassword
+            })
         })
+            .then(res => res.json())
+            .then(resJson => {
+                if (resJson === "ok") {
+                    usersData.push(userCardComponent({
+                        id: usersData.length + 1,
+                        name: userName
+                    }))
+                rootElement.insertAdjacentHTML("beforeend", usersData[usersData.length - 1])
+                }
+            })
     })
+
+    fetch("/users")
         .then(res => res.json())
-        .then(resJson => console.log(resJson))
-})
+        .then (data => {
+            usersData = data.map(user => userCardComponent(user))
+            console.log(usersData)
+            rootElement.insertAdjacentHTML("beforeend", usersData.join(""))
+        })
+}
+
+init()
